063dd29c9c753050ad601b67353c9ce1480ef26c
Victoria Hale <vic.hale74@gmail.com>
2019-10-14 20:27:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,5 +8,14 @@ class Person
   def name=(persons_name)
     @name = persons_name 
   end
+  def initialize 
+    @job = job 
+  end 
+  def job 
+    @job 
+  end
+  def job=(persons_job)
+    @job = persons_job
+  end
     
 end 
