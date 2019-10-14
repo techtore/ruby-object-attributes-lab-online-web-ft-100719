97a5ea0d5c9470feca28021ae71821d0feaa7995
Victoria Hale <vic.hale74@gmail.com>
2019-10-14 20:27:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+  def initialize
+    @name = name
+  end 
+  def name 
+    @name
+  end
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  def initialize
+    @breed = breed 
+  end
+  def breed 
+    @breed 
+  end 
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person 
+  def initialize 
+    @name = name
+  end
+  def name 
+    @name 
+  end 
+  def name=(persons_name)
+    @name = persons_name 
+  end
+  def initialize 
+    @job = job 
+  end 
+  def job 
+    @job 
+  end
+  def job=(persons_job)
+    @job = persons_job
+  end
+    
+end 
